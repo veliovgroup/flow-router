45fa8266b88fed489bccae022fb56fc0e406dff3
dr.dimitru <ceo@veliov.com>
2017-02-23 14:44:46 +0300

    v2.12.9
     - Fix for cases when route's param isn't String
     - Fix broken redirects in triggers
     - Fix exception on `new Route()` without arguments
     - Preserve `%` and `+` symbols in URI
     - Allow to use triggersEnter in Group without Array, see [PR
    #659](https://github.com/kadirahq/flow-router/pull/659)
     - Fix generation of URLs with a ROOT_URL containing a path, see [PR
    #691](https://github.com/kadirahq/flow-router/pull/691)
     - Support RegEx ([0-9]*) in .path, see [PR
    #654](https://github.com/kadirahq/flow-router/pull/654)
     - Fix nested groups on server, see [PR
    #663](https://github.com/kadirahq/flow-router/pull/663)
     - Same behavior for Server groups, as for groups on Client
     - Add auto-tests
     - NPM Dependencies update

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ostrio:flow-router-extra',
   summary: 'Carefully extended flow-router with waitOn and template context',
-  version: '2.12.7',
+  version: '2.12.9',
   git: 'https://github.com/VeliovGroup/flow-router'
 });
 
@@ -39,9 +39,37 @@ Package.onUse(function (api) {
   api.addFiles('lib/router.js', ['client', 'server']);
 
   api.export('FlowRouter');
+  // Uncomment before running tests
+  // api.export('Triggers');
+});
+
+Package.onTest(function(api) {
+  api.use(['tinytest', 'underscore', 'reactive-var', 'tracker', 'check', 'mongo', 'http', 'random', 'ostrio:flow-router-extra', 'meteorhacks:fast-render', 'meteorhacks:inject-data', 'tmeasday:html5-history-api']);
+
+  api.addFiles('test/common/fast_render_route.js', ['client', 'server']);
+
+  api.addFiles('test/client/_helpers.js', 'client');
+  api.addFiles('test/server/_helpers.js', 'server');
+
+  api.addFiles('test/client/loader.spec.js', 'client');
+  api.addFiles('test/client/route.reactivity.spec.js', 'client');
+  api.addFiles('test/client/router.core.spec.js', 'client');
+  api.addFiles('test/client/router.subs_ready.spec.js', 'client');
+  api.addFiles('test/client/router.reactivity.spec.js', 'client');
+  api.addFiles('test/client/group.spec.js', 'client');
+  api.addFiles('test/client/trigger.spec.js', 'client');
+  api.addFiles('test/client/triggers.js', 'client');
+
+  api.addFiles('test/server/plugins/fast_render.js', 'server');
+
+  api.addFiles('test/common/router.path.spec.js', ['client', 'server']);
+  api.addFiles('test/common/router.url.spec.js', ['client', 'server']);
+  api.addFiles('test/common/router.addons.spec.js', ['client', 'server']);
+  api.addFiles('test/common/route.spec.js', ['client', 'server']);
+  api.addFiles('test/common/group.spec.js', ['client', 'server']);
 });
 
 Npm.depends({
   'page': '1.7.1',
-  'qs': '6.3.0'
+  'qs': '6.3.1'
 });

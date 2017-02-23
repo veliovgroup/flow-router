4c9e746131922c940e938aef3884ce2e747b723e
dr.dimitru <ceo@veliov.com>
2017-02-23 13:57:48 +0300

    Add auto-tests

---
+++ test/client/_helpers.js
@@ -0,0 +1,11 @@
+Package['kadira:flow-router'] = Package['ostrio:flow-router-extra'];
+GetSub = function (name) {
+  for(var id in Meteor.connection._subscriptions) {
+    var sub = Meteor.connection._subscriptions[id];
+    if(name === sub.name) {
+      return sub;
+    }
+  }
+};
+
+FlowRouter.route('/');
---
+++ test/common/route.spec.js
@@ -0,0 +1,16 @@
+Package['kadira:flow-router'] = Package['ostrio:flow-router-extra'];
+Router = FlowRouter.Router;
+
+Tinytest.addAsync('Common - Route - expose route options', function (test, next) {
+  var pathDef = "/" + Random.id();
+  var name = Random.id();
+  var data = {aa: 10};
+  
+  FlowRouter.route(pathDef, {
+    name: name,
+    someData: data
+  });
+
+  test.equal(FlowRouter._routesMap[name].options.someData, data);
+  next();
+});

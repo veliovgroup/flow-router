7ec278618f8237693d85554b9ad301b1b0b7467d
dr.dimitru <ceo@veliov.com>
2017-11-11 00:19:20 +0300

    v3.4.1
     - Dependencies update
     - Compatibility with `meteor@1.6`
     - Fix #32 , thanks to @dylanmcgowan
     - Implement #30 , add support for all hooks into `Group` definition.
    Thanks to @THETCR

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ostrio:flow-router-extra',
   summary: 'Carefully extended flow-router with waitOn and template context',
-  version: '3.4.0',
+  version: '3.4.1',
   git: 'https://github.com/VeliovGroup/flow-router'
 });
 

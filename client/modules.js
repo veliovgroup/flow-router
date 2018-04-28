0b8cb8843908ccf848dc07d153d01b62c6a83fd7
dr.dimitru <ceo@veliov.com>
2018-04-28 04:30:35 +0300

    ES6

--- client/modules.js
+++ client/modules.js
@@ -1,5 +1,5 @@
-const page = require('page');
-const qs   = require('qs');
+import page from 'page';
+import qs   from 'qs';
 const requestAnimFrame = (() => {
   return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
     setTimeout(callback, 1000 / 60);

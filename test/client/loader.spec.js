c993328be736983f24df1a03ebf5f76554665fed
dr.dimitru <ceo@veliov.com>
2018-07-26 12:22:47 +0300

    Minor tests suite update

--- test/client/loader.spec.js
+++ test/client/loader.spec.js
@@ -2,14 +2,14 @@ import { FlowRouter, Router } from 'meteor/ostrio:flow-router-extra';
 
 Tinytest.add('Client - import page.js', function (test) {
   test.isTrue(!!Router.prototype._page);
+  test.isTrue(!!FlowRouter._page);
   test.isFalse(!!window.page);
 });
 
-
 Tinytest.add('Client - import query.js', function (test) {
   test.isTrue(!!Router.prototype._qs);
+  test.isTrue(!!FlowRouter._qs);
 });
-
 
 Tinytest.add('Client - create FlowRouter', function (test) {
   test.isTrue(!!FlowRouter);

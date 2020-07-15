b5635754e9e46e4509d2ecb556af00051ca10570
dr.dimitru <ceo@veliov.com>
2020-07-15 14:55:52 +0300

    📦 v3.7.5
    - ✨ Add global `.onRenderError` callback, see [updated
    docs](https://github.com/VeliovGroup/flow-router/blob/master/docs/api/re
    nder.md#render-method);
    - 👨‍💻 Use callback if template or layout not found in `.render()`
    method; Old behavior: Throw an exception;

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ostrio:flow-router-extra',
   summary: 'Carefully extended flow-router with waitOn and template context',
-  version: '3.7.4',
+  version: '3.7.5',
   git: 'https://github.com/VeliovGroup/flow-router'
 });
 

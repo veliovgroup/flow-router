0e9e44d351e7079d362294d9cfec36443c6bc6a3
dr.dimitru <ceo@veliov.com>
2025-01-26 09:10:12 +0200

    📦 v3.11.0
    - Support for `meteor@3`
    - TypeScript definitions
    - Thanks to @TheRuggster @harveysanders @jankapunkt @harryadel for contributions, tests, reviews, and pushing this release forward

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'ostrio:flow-router-extra',
   summary: 'Carefully extended flow-router with waitOn and template context',
-  version: '3.11.0-rc300.1',
-  git: 'https://github.com/veliovgroup/flow-router'
+  version: '3.11.0',
+  git: 'https://github.com/veliovgroup/flow-router',
 });
 
 Package.onUse((api) => {
-  api.versionsFrom(['1.4', '3.0-rc.0']);
+  api.versionsFrom(['1.4', '2.8.0', '3.0.1']);
   api.use(['modules', 'ecmascript', 'promise', 'tracker', 'reactive-dict', 'reactive-var', 'ejson', 'check'], ['client', 'server']);
 
-  api.use(['zodern:types@1.0.13', 'typescript@4.9.5 || 5.4.3-rc300.0'], ['client', 'server'], { weak: true });
-  api.use(['templating', 'blaze@2.7.1 || 2.8.0 || 2.9.0 || 3.0.0-alpha300.0'], 'client', { weak: true });
+  api.use(['zodern:types@1.0.13', 'typescript'], ['client', 'server'], { weak: true });
+  api.use(['templating', 'blaze@2.0.0 || 3.0.0'], 'client', { weak: true });
   api.mainModule('client/_init.js', 'client');
   api.mainModule('server/_init.js', 'server');
 
@@ -19,10 +19,14 @@ Package.onUse((api) => {
 });
 
 Package.onTest((api) => {
-  api.use(['ecmascript', 'tinytest', 'underscore', 'check', 'mongo', 'http', 'random', 'communitypackages:fast-render@4.0.6', 'communitypackages:inject-data@2.3.2', 'montiapm:meteorx@2.3.1', 'ostrio:flow-router-extra', 'zodern:types', 'typescript@4.9.5 || 5.4.3-rc300.0'], ['client', 'server']);
+  api.use(['ecmascript', 'tinytest', 'underscore', 'check', 'mongo', 'http', 'random', 'ostrio:flow-router-extra', 'zodern:types', 'typescript'], ['client', 'server']);
   api.use(['reactive-var', 'tracker'], 'client');
 
-  api.addFiles('test/common/fast_render_route.js', ['client', 'server']);
+  // Temporary disable `fast-render` tests as not compatible with meteor@3
+  // once fast-render, meteorx, and inject-data are compatible with meteor@3, add the next packages:
+  // 'communitypackages:fast-render', 'communitypackages:inject-data', 'montiapm:meteorx'
+  // api.addFiles('test/common/fast_render_route.js', ['client', 'server']);
+  // api.addFiles('test/server/plugins/fast_render.js', 'server');
 
   api.addFiles('test/client/_helpers.js', 'client');
   api.addFiles('test/server/_helpers.js', 'server');
@@ -36,7 +40,6 @@ Package.onTest((api) => {
   api.addFiles('test/client/trigger.spec.js', 'client');
   api.addFiles('test/client/triggers.js', 'client');
 
-  api.addFiles('test/server/plugins/fast_render.js', 'server');
 
   api.addFiles('test/common/router.path.spec.js', ['client', 'server']);
   api.addFiles('test/common/router.url.spec.js', ['client', 'server']);
@@ -47,5 +50,5 @@ Package.onTest((api) => {
 
 Npm.depends({
   page: '1.9.0',
-  qs: '6.11.0'
+  qs: '6.11.0',
 });

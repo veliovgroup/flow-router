979a53cba64a6b363f3b0c4ccd64e5276b99d1ce
dupontbertrand <dupontbertrand59@gmail.com>
2026-03-11 20:07:51 +0100

    security: remove publicly exposed ___refresh route
    The /___refresh/:layout/:template/:oldRoute? route was registered
    automatically at startup with no authentication or validation.
    
    It called JSON.parse(queryParams.oldParams) on untrusted user input,
    and passed :layout and :template params directly to this.render()
    without checking they correspond to existing templates.
    
    Use FlowRouter.reload() to reload the current route instead.
    
    BREAKING CHANGE: FlowRouter.refresh(layout, template) is removed.

--- client/_init.js
+++ client/_init.js
@@ -5,8 +5,6 @@ import Group         from './group.js';
 import Triggers      from './triggers.js';
 import BlazeRenderer from './renderer.js';
 import helpersInit   from './active.route.js';
-import { _helpers }  from './../lib/_helpers.js';
-import { requestAnimFrame } from './modules.js';
 
 if (Package['zimme:active-route']) {
   Meteor._debug('Please remove `zimme:active-route` package, as its features is build into flow-router-extra, and will interfere.');
@@ -51,50 +49,6 @@ Meteor.startup(() => {
   if(!FlowRouter._askedToWait && !FlowRouter._initialized) {
     FlowRouter.initialize();
 
-    FlowRouter.route('/___refresh/:layout/:template/:oldRoute?', {
-      name: '___refresh',
-      action(params, queryParams) {
-        this.render(params.layout, params.template, () => {
-          requestAnimFrame(() => {
-            if (params.oldRoute) {
-              try {
-                if (history.length) {
-                  window.history.go(-1);
-                } else {
-                  FlowRouter.go(params.oldRoute, (queryParams.oldParams ? JSON.parse(queryParams.oldParams) : {}));
-                }
-              } catch (e) {
-                FlowRouter.go('/');
-              }
-            } else {
-              if (history.length) {
-                window.history.go(-1);
-              } else {
-                FlowRouter.go('/');
-              }
-            }
-          });
-        });
-      }
-    });
-
-    FlowRouter.refresh = (layout, template) => {
-      if (!layout || !_helpers.isString(layout)) {
-        throw new Meteor.Error(400, '[FlowRouter.refresh(layout, template)] -> "layout" must be a String!');
-      }
-
-      if (!template || !_helpers.isString(template)) {
-        throw new Meteor.Error(400, '[FlowRouter.refresh(layout, template)] -> "template" must be a String!');
-      }
-
-      FlowRouter.go('___refresh', {
-        oldRoute: FlowRouter._current.route.name,
-        layout,
-        template
-      }, {
-        oldParams: JSON.stringify(FlowRouter._current.params || {})
-      });
-    };
   }
 });
 
